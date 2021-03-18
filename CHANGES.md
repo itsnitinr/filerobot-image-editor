bec20d47339dde4704cb5a5b65f666f2e6e1b6f6
Nitin Ranganath <nitinranganath@gmail.com>
2021-03-18 13:37:28 +0530

    Change watermark select to button

---
+++ CHANGES.md
@@ -0,0 +1,7 @@
+## Change Watermark Image Select to Button
+
+- Go to `src/components/Toolbar/Watermark.js`
+- Go to `line 453`
+- Edit the contents of `<WrapperForURL>` component
+- Replace it with a button to open modal
+- Build `Modal` component in `src/components/Shared/Modal.js`
---
+++ src/components/Shared/Modal.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import './Modal.css';
+
+class Modal extends Component {
+  render() {
+    return (
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <h2>Here i am</h2>
+          <a className="close" href="#">
+            &times;
+          </a>
+          <div className="content">
+            Thank to pop me out of that button, but now i'm done so you can
+            close this window.
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
